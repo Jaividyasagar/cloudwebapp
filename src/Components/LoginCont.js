afe8184dd41bd5cfd1c40bc465274286ad590370
Jaividyasagar <88584859+Jaividyasagar@users.noreply.github.com>
2023-04-22 10:59:18 +0530

    Update

--- src/Components/LoginCont.js
+++ src/Components/LoginCont.js
@@ -25,6 +25,7 @@ const LoginCont = () => {
                 6. Now you will see the uploaded files in the home section<br></br>
                 7. You can download or delete the particular file whenever you want.<br></br>
             </p>
+            <iframe className='ytapk' width="560" height="315" src="https://www.youtube.com/embed/QaCfOatTIDA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
         <div id="contactLine"></div>
         <div class="contact-container">
